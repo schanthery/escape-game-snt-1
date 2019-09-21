009c5d651a90c7c5d87a95e4c60da350e9eba09e
schanthery <schanthery@yahoo.fr>
2019-09-21 21:00:19 +0200

    seb
    seb

--- scripts/menu.js
+++ scripts/menu.js
@@ -44,6 +44,17 @@ var menu = function (game) {
     var python_bug;
     var python_ok;
     var bt_anneau;
+    var bt_digicode_gandalf;
+    var bt_digicode_gandalf_on;
+    var digicode_gandalf;
+    var digicode_gandalf_off;
+    var bt_gandalf;
+    var bt_croix_gandalf;
+    var zone_gandalf;
+    var bt_ok_gandalf;
+    var anneau_dos;
+    var bt_croix_anneau;
+    var bt_digicode_gandalf_on;
 }
 menu.prototype = {
     preload: function () {
@@ -84,6 +95,13 @@ menu.prototype = {
         this.game.load.image("python_bug","img/python_bug.png");
         this.game.load.image("python_ok","img/python_ok.png");
         this.game.load.spritesheet("bt_anneau","img/bt_anneau.png",155,111);
+        this.game.load.spritesheet("bt_digicode_gandalf","img/bt_digicode_gandalf.png",40,50);
+        this.game.load.spritesheet("bt_digicode_gandalf_on","img/bt_digicode_gandalf_on.png",40,50);
+        this.game.load.image("digicode_gandalf","img/digicode_gandalf.png");
+        this.game.load.image("digicode_gandalf_off","img/digicode_gandalf_off.png");
+        this.game.load.spritesheet("bt_gandalf","img/bt_gandalf.png",130,130);
+        this.game.load.image("anneau_dos","img/anneau_dos.png");
+      
     },
     create:function() {
         // Center game canvas on page
@@ -107,38 +125,20 @@ menu.prototype = {
         this.bt_matrix=this.game.add.button(635,0,"bt_matrix",this.bt_matrix,this,1,0,2,0);
         // bt_anneau
         this.bt_anneau=this.game.add.button(186,138,"bt_anneau",this.bt_anneau,this,1,0,2,0);
+        // bt_digicode_gandalf
+        this.bt_digicode_gandalf=this.game.add.button(457,340,"bt_digicode_gandalf",this.bt_digicode_gandalf,this,1,0,2,0);
+        // bt_digicode_gandalf_on
+        this.bt_digicode_gandalf_on=this.game.add.button(457,340,"bt_digicode_gandalf_on",this.bt_digicode_gandalf_on,this,1,0,2,0);
+        this.bt_digicode_gandalf_on.visible=false;
         // création zone clavier porte
         this.bouton_clavier = this.game.add.button(580,345, 'clavier_sheet', this.bouton_clavier,this,1,0,2,0);
-        this.clavier_menu=this.game.add.image(350,100,'clavier_menu')
-        this.clavier_menu.visible=false;
-        this.bouton_croix_clavier_menu=this.game.add.button(705,110,"croix_sheet",this.croix_menu_clavier,this,1,0,2,0)
-        this.bouton_croix_clavier_menu.visible=false;
-        this.bouton_valider_clavier_menu=this.game.add.button(595,505,"bt_valider_clavier_menu",this.bt_valider_clavier_menu,this,1,0,2,0)
-        this.bouton_valider_clavier_menu.visible=false;
-        this.texte_clavier_menu=this.game.add.text(385, 550, "Ce n'est pas le bon code", this.style_bleu);
-        this.texte_clavier_menu.visible=false
-        this.zone_clavier_menu = game.add.inputField(520, 505, {
-            font: '18px Arial',
-            fill: '#000000',
-            fillAlpha: 100,
-            fontWeight: 'bold',
-            width: 50,
-            max: 4,
-            padding: 8,
-            borderWidth: 1,
-            borderColor: '#000000',
-            borderRadius: 6,
-            placeHolder: '????',
-            textAlign: 'center',
-            zoom: true,
-        });
-        this.zone_clavier_menu.visible=false;
+
         // zone lecteur de carte
         this.bt_pc1=this.game.add.button(215,565,"bt_pc1",this.bt_pc1,this,1,0,2,0)
         this.bouton_lecteur=this.game.add.button(120,690,"bt_lecteur",this.bt_lecteur,this,1,0,2,0)
-        this.menu_lecteur=this.game.add.image(350,100,'menu_lecteur');
-        this.bouton_carte=this.game.add.button(490,250,"bt_carte",this.bt_carte,this,1,0,2,0);
-        this.bouton_croix_lecteur_menu=this.game.add.button(705,110,"croix_sheet",this.bouton_croix_lecteur_menu,this,1,0,2,0)
+        this.menu_lecteur=this.game.add.image(0,0,'menu_lecteur');
+        this.bouton_carte=this.game.add.button(450,340,"bt_carte",this.bt_carte,this,1,0,2,0);
+        this.bouton_croix_lecteur_menu=this.game.add.button(660,200,"croix_sheet",this.bouton_croix_lecteur_menu,this,1,0,2,0)
         this.menu_lecteur.visible=false;
         this.bouton_croix_lecteur_menu.visible=false;
         this.bouton_carte.visible=false;
@@ -189,6 +189,30 @@ menu.prototype = {
         this.tv.visible=false;
         this.bt_croix_tv=this.game.add.button(980,5,"croix_sheet",this.bt_croix_tv,this,1,0,2,0);
         this.bt_croix_tv.visible=false;
+        this.clavier_menu=this.game.add.image(0,0,'clavier_menu')
+        this.clavier_menu.visible=false;
+        this.bouton_croix_clavier_menu=this.game.add.button(660,145,"croix_sheet",this.croix_menu_clavier,this,1,0,2,0)
+        this.bouton_croix_clavier_menu.visible=false;
+        this.bouton_valider_clavier_menu=this.game.add.button(555,535,"bt_valider_clavier_menu",this.bt_valider_clavier_menu,this,1,0,2,0)
+        this.bouton_valider_clavier_menu.visible=false;
+        this.texte_clavier_menu=this.game.add.text(385, 550, "Ce n'est pas le bon code", this.style_bleu);
+        this.texte_clavier_menu.visible=false
+        this.zone_clavier_menu = game.add.inputField(480, 535, {
+            font: '18px Arial',
+            fill: '#000000',
+            fillAlpha: 100,
+            fontWeight: 'bold',
+            width: 50,
+            max: 4,
+            padding: 8,
+            borderWidth: 1,
+            borderColor: '#000000',
+            borderRadius: 6,
+            placeHolder: '????',
+            textAlign: 'center',
+            zoom: true,
+        });
+        this.zone_clavier_menu.visible=false;
         // pc2
         this.pc2_on=this.game.add.image(0,0,"pc2_on");
         this.bt_croix_pc2=this.game.add.button(980,15,"croix_sheet",this.bt_croix_pc2,this,1,0,2,0);
@@ -224,8 +248,32 @@ menu.prototype = {
             this.icon[i].visible=false;
             this.icon_text[i].visible=false;
         }
+                this.clavier_menu=this.game.add.image(0,0,'clavier_menu')
+        this.clavier_menu.visible=false;
+        this.bouton_croix_clavier_menu=this.game.add.button(660,145,"croix_sheet",this.croix_menu_clavier,this,1,0,2,0)
+        this.bouton_croix_clavier_menu.visible=false;
+        this.bouton_valider_clavier_menu=this.game.add.button(555,535,"bt_valider_clavier_menu",this.bt_valider_clavier_menu,this,1,0,2,0)
+        this.bouton_valider_clavier_menu.visible=false;
+        this.texte_clavier_menu=this.game.add.text(385, 550, "Ce n'est pas le bon code", this.style_bleu);
+        this.texte_clavier_menu.visible=false
+        this.zone_clavier_menu = game.add.inputField(480, 535, {
+            font: '18px Arial',
+            fill: '#000000',
+            fillAlpha: 100,
+            fontWeight: 'bold',
+            width: 50,
+            max: 4,
+            padding: 8,
+            borderWidth: 1,
+            borderColor: '#000000',
+            borderRadius: 6,
+            placeHolder: '????',
+            textAlign: 'center',
+            zoom: true,
+        });
+        this.zone_clavier_menu.visible=false;
         // cadre 1
-        this.cadre1=this.game.add.image(200,0,"cadre1");
+        this.cadre1=this.game.add.image(0,0,"cadre1");
         this.bt_croix_cadre1=this.game.add.button(784,4,"croix_sheet",this.bt_croix_cadre1,this,1,0,2,0);
         this.bt_croix_cadre1.visible=false;
         this.cadre1.visible=false;
@@ -259,6 +307,67 @@ menu.prototype = {
         this.pc2_python.visible=false
         this.bt_croix_pc2_python=this.game.add.button(990,4,"croix_sheet",this.bt_croix_pc2_python,this,1,0,2,0);
         this.bt_croix_pc2_python.visible=false;
+        // gandalf digicode
+        this.digicode_gandalf=this.game.add.image(0,0,"digicode_gandalf");
+        this.digicode_gandalf.visible=false;
+        this.digicode_gandalf_off=this.game.add.image(0,0,"digicode_gandalf_off");
+        this.digicode_gandalf_off.visible=false;
+        this.bt_gandalf=this.game.add.button(450,180,"bt_gandalf",this.bt_gandalf,this,1,0,2,0)
+        this.bt_croix_gandalf=this.game.add.button(990,4,"croix_sheet",this.bt_croix_gandalf,this,1,0,2,0);
+        this.zone_gandalf = game.add.inputField(450, 385, {
+            font: '20px Arial',
+            fill: '#000000',
+            fillAlpha: 1,
+            fontWeight: 'bold',
+            width: 100,
+            max: 4,
+            padding: 8,
+            borderWidth: 1,
+            borderColor: '#000000',
+            borderRadius: 0,
+            placeHolder: ' ',
+            placeHolderColor:'#000000',
+            textAlign: 'center',
+            zoom: true,
+        });
+        this.bt_ok_gandalf=this.game.add.button(570,382,"bt_ok",this.bt_ok_gandalf,this,1,0,2,0)
+        this.digicode_gandalf.visible=false;
+        this.bt_gandalf.visible=false;
+        this.bt_croix_gandalf.visible=false;
+        this.bt_ok_gandalf.visible=false;
+        this.zone_gandalf.visible=false;
+        // anneau_dos
+        this.anneau_dos=this.game.add.image(0,0,"anneau_dos");
+        this.bt_croix_anneau=this.game.add.button(975,15,"croix_sheet",this.bt_croix_anneau,this,1,0,2,0);
+        this.anneau_dos.visible=false;
+        this.bt_croix_anneau.visible=false;
+        // menu clavier
+        this.clavier_menu=this.game.add.image(0,0,'clavier_menu')
+        this.clavier_menu.visible=false;
+        this.bouton_croix_clavier_menu=this.game.add.button(660,145,"croix_sheet",this.croix_menu_clavier,this,1,0,2,0)
+        this.bouton_croix_clavier_menu.visible=false;
+        this.bouton_valider_clavier_menu=this.game.add.button(555,535,"bt_valider_clavier_menu",this.bt_valider_clavier_menu,this,1,0,2,0)
+        this.bouton_valider_clavier_menu.visible=false;
+        this.texte_clavier_menu=this.game.add.text(385, 550, "Ce n'est pas le bon code", this.style_bleu);
+        this.texte_clavier_menu.visible=false
+        this.zone_clavier_menu = game.add.inputField(430, 535, {
+            font: '18px Arial',
+            fill: '#000000',
+            fillAlpha: 100,
+            fontWeight: 'bold',
+            width: 100,
+            max: 9,
+            padding: 8,
+            borderWidth: 1,
+            borderColor: '#000000',
+            borderRadius: 6,
+            placeHolder: '$$$$$$$$$',
+            textAlign: 'center',
+            zoom: true,
+        });
+        this.zone_clavier_menu.visible=false;
+        // alumage des boutons
+        this.boutons_on()
         
 
     },
@@ -268,6 +377,7 @@ menu.prototype = {
 
     bouton_clavier:function() {
         console.log(' clavier down');
+        this.boutons_off()
         this.clavier_menu.visible=true
         this.bouton_croix_clavier_menu.visible=true
         this.zone_clavier_menu.visible=true;
@@ -279,16 +389,20 @@ menu.prototype = {
         this.zone_clavier_menu.visible=false;
         this.bouton_valider_clavier_menu.visible=false;
         this.texte_clavier_menu.visible=false
+        this.boutons_on()
     },
     bt_valider_clavier_menu:function(){
-        if (this.zone_clavier_menu.value=="1234"){
+        if (this.zone_clavier_menu.value=="281576461"){
             this.texte_clavier_menu.visible=false
             this.clavier_menu.visible=false;
             this.bouton_croix_clavier_menu.visible=false;
             this.zone_clavier_menu.visible=false;
             this.bouton_valider_clavier_menu.visible=false;
             this.texte_clavier_menu.visible=false
-            var tween = this.game.add.tween(porte).to( { x: [492,600 ]}, 5000, "Sine.easeInOut", true, false);
+            this.bt_digicode_gandalf_on.visible=true;
+            this.bt_digicode_gandalf.visible=false;
+            this.boutons_on()
+            
 
         }else{
             this.texte_clavier_menu.visible=true
@@ -296,16 +410,17 @@ menu.prototype = {
     },
     bt_lecteur:function(){
         this.menu_lecteur.visible=true;
-       this.bouton_croix_lecteur_menu.visible=true;
-       this.bouton_carte.visible=true;
+        this.bouton_croix_lecteur_menu.visible=true;
+        this.bouton_carte.visible=true;
+        this.boutons_off()
     },
     bt_carte:function(){
         //this.menu_lecteur.visible=false;
         //this.bouton_croix_lecteur_menu.visible=false;
         //this.bouton_carte.visible=false;
-        console.log("toto");
         var url="img/carte_code.png"
         var win = window.open(url, '_blank');
+
         
         
     },
@@ -313,6 +428,7 @@ menu.prototype = {
        this.menu_lecteur.visible=false;
        this.bouton_croix_lecteur_menu.visible=false;
        this.bouton_carte.visible=false;
+       this.boutons_on()
       
        
 
@@ -327,6 +443,7 @@ menu.prototype = {
         this.ecran_pc1.visible=true;
         this.zone_ecran_pc1.visible=true;
         this.bt_ok.visible=true;
+        this.boutons_off()
      
     },
 	bouton_croix_ecran_pc1:function(){
@@ -335,9 +452,10 @@ menu.prototype = {
         this.bouton_croix_ecran_pc1.visible=false;
         this.zone_ecran_pc1.visible=false;
         this.bt_ok.visible=false;
+        this.boutons_on()
     },
     bt_ok:function(){
-        if (this.zone_ecran_pc1.value=="visicalc"){
+        if (this.zone_ecran_pc1.value=="VISICALC"){
             this.bt_croix_ecran1.visible=true
             this.bt_download.visible=true
             this.ecran1.visible=true
@@ -348,6 +466,7 @@ menu.prototype = {
         this.placard.visible=true;
         this.bt_codex.visible=true;
         this.bt_croix_placard.visible=true;
+        this.boutons_off()
     },
     bt_codex:function(){
         var url="img/codex.png"
@@ -357,6 +476,7 @@ menu.prototype = {
         this.placard.visible=false;
         this.bt_codex.visible=false;
         this.bt_croix_placard.visible=false;
+        this.boutons_on()
     },
     bt_download:function(){
         var url="img/boxoffice.csv"
@@ -371,18 +491,21 @@ menu.prototype = {
         this.bouton_croix_ecran_pc1.visible=false;
         this.zone_ecran_pc1.visible=false;
         this.bt_ok.visible=false;
+        this.boutons_on()
     },
     bt_tv:function(){
         this.tv.visible=true;
         this.bt_croix_tv.visible=true;
+        this.boutons_off()
     },
     bt_croix_tv:function(){
         this.tv.visible=false
         this.bt_croix_tv.visible=false;
+        this.boutons_on()
 
     },
     pc2:function(){
-        
+        this.boutons_off()
         this.bt_croix_pc2.visible=true;
         this.pc2_on.visible=true
         this.bt_ok_pc2.visible=true;
@@ -393,6 +516,7 @@ menu.prototype = {
         }
     },
     bt_croix_pc2:function(){
+        this.boutons_on()
         this.bt_croix_pc2.visible=false;
         this.pc2_on.visible=false;
         this.bt_ok_pc2.visible=false;
@@ -442,15 +566,18 @@ menu.prototype = {
             this.zone_pc2_python.visible=true;
             this.pc2_python.visible=true
             this.bt_croix_pc2_python.visible=true;
+            this.boutons_on()
         }
     },
     bt_cadre1:function(){
         this.bt_croix_cadre1.visible=true;
         this.cadre1.visible=true;
+        this.boutons_off()
     },
     bt_croix_cadre1:function(){
         this.bt_croix_cadre1.visible=false;
         this.cadre1.visible=false;
+        this.boutons_on()
     },
     bt_matrix:function(){
         var url="https://youtu.be/8xx91zoASLY"
@@ -473,8 +600,85 @@ menu.prototype = {
         this.bt_run.visible=false;
         this.pc2_python.visible=false;
         this.bt_croix_pc2_python.visible=false;
+        this.boutons_on()
     },
     bt_anneau:function(){
+        this.bt_croix_anneau.visible=true;
+        this.anneau_dos.visible=true;
+        this.boutons_off()
+    },
+    bt_digicode_gandalf_on:function(){
+        this.digicode_gandalf.visible=true;
+        this.bt_gandalf.visible=true;
+        this.bt_croix_gandalf.visible=true;
+        this.bt_ok_gandalf.visible=true;
+        this.zone_gandalf.visible=true;
+        this.boutons_off()
+    },
+    bt_digicode_gandalf:function(){
+        this.digicode_gandalf_off.visible=true;
+        this.digicode_gandalf.visible=false;
+        this.bt_croix_gandalf.visible=true;
+        this.boutons_off()
+        //this.bt_ok_gandalf.visible=true;
+        //this.zone_gandalf.visible=true;
+    },
+    bt_gandalf:function(){
+        var url="https://youtu.be/Ygnez_odlNg"
+        var win = window.open(url, '_blank');
+    },
+    bt_croix_gandalf:function(){
+        this.digicode_gandalf.visible=false;
+        this.bt_gandalf.visible=false;
+        this.bt_croix_gandalf.visible=false
+        this.bt_ok_gandalf.visible=false;
+        this.zone_gandalf.visible=false;
+        this.digicode_gandalf_off.visible=false;
+        this.boutons_on()
+    },
+    bt_ok_gandalf:function(){
+        if (this.zone_gandalf.value=="7564"){
+            this.digicode_gandalf.visible=false;
+            this.bt_gandalf.visible=false;
+            this.bt_croix_gandalf.visible=false
+            this.bt_ok_gandalf.visible=false;
+            this.zone_gandalf.visible=false;
+            var tween = this.game.add.tween(porte).to( { x: [492,600 ]}, 5000, "Sine.easeInOut", true, false);
+            this.boutons_on()
+        }
 
+    },
+    bt_croix_anneau:function(){
+        this.bt_croix_anneau.visible=false;
+        this.anneau_dos.visible=false;
+        this.boutons_on()
+    },
+    boutons_off(){
+        this.bt_tv.inputEnabled=false;
+        this.bt_placard.inputEnabled=false;
+        this.bt_cadre1.inputEnabled=false;
+        this.placard.inputEnabled=false;
+        this.bt_anneau.inputEnabled=false;
+        this.bt_digicode_gandalf.inputEnabled=false;
+        this.bt_digicode_gandalf_on.inputEnabled=false;
+        this.bt_pc1.inputEnabled=false;
+        this.bt_matrix.inputEnabled=false;
+        this.bouton_clavier.inputEnabled=false;
+        this.pc2.inputEnabled=false;
+        this.bouton_lecteur.inputEnabled=false;
+    },
+    boutons_on(){
+        this.bt_tv.inputEnabled=true;
+        this.bt_placard.inputEnabled=true;
+        this.bt_cadre1.inputEnabled=true;
+        this.placard.inputEnabled=true;
+        this.bt_anneau.inputEnabled=true;
+        this.bt_digicode_gandalf.inputEnabled=true;
+        this.bt_digicode_gandalf_on.inputEnabled=true;
+        this.bt_pc1.inputEnabled=true;
+        this.bt_matrix.inputEnabled=true;
+        this.bouton_clavier.inputEnabled=true;
+        this.pc2.inputEnabled=true;
+        this.bouton_lecteur.inputEnabled=true;
     }
 }
